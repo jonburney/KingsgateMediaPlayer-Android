463110669f7ebdb7c7a18a1690ad625ca8f3a7b6
Jon Burney <jburney@microsoft.com>
2016-02-03 21:56:50 +0000

    Initial commit of the skeleton app showing background and static list ListView

---
+++ app/src/main/java/jonburney/version7/kingsgatemediaplayer/HomeActivity.java
@@ -0,0 +1,54 @@
+package jonburney.version7.kingsgatemediaplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ListView videoList = (ListView) findViewById(R.id.videoList);
+        ArrayList<String> listItems = new ArrayList<String>() {{
+            add("Test Video 1");
+            add("Test Video 2");
+        }};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        videoList.setAdapter(adapter);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
